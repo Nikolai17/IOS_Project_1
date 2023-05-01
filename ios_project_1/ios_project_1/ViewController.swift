b450c78a385e4f1a2713146bb647deaeeedc97a3
Nikolay Volnikov <nvolnikov@rencredit.ru>
2023-05-01 11:58:08 +0300

    branch1: create new project

---
+++ ios_project_1/ios_project_1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ios_project_1
+//
+//  Created by Nikolay Volnikov on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
